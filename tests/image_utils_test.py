fad8a7f9225ce6afa2b3b2a4c774dbe62e23f932
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-15 12:10:19 +0200

    fix image utils test, add parametrized test data

--- tests/image_utils_test.py
+++ tests/image_utils_test.py
@@ -1,50 +1,45 @@
 import pytest
-from api.image_utils import ImageUtils
+from api.utils import ImageUtils
 
+testdata = [
+    # original_width, original_height, new_width, new_height
+    (2048, 1536, 512, 384),
+    (1536, 2048, 384, 512),
+    (512, 512, 256, 256),
+    (10, 2, 5, 1),
+    (2, 10, 1, 5),
+]
 
-def test_image_dimensions_should_be_scaled_correctly_with_width():
-    original_width, original_height = 2048, 1536
-    new_width, expected_height = 512, 384
+@pytest.mark.parametrize("original_width, original_height, new_width, new_height", testdata)
+def test_image_dimensions_should_be_scaled_correctly_with_width(original_width, original_height, new_width, new_height):
+    expected_height = new_height
 
     width, height = ImageUtils.calculate_scaled_size(
         original_width=original_width, original_height=original_height, width=new_width
     )
-    assert height == expected_height
     assert width == new_width
+    assert height == expected_height
 
 
-def test_image_dimensions_should_be_scaled_correctly_with_height():
-    original_width, original_height = 1536, 2048
-    expected_width, new_height = 384, 512
+@pytest.mark.parametrize("original_width, original_height, new_width, new_height", testdata)
+def test_image_dimensions_should_be_scaled_correctly_with_height(original_width, original_height, new_width, new_height):
+    expected_width = new_width
 
     width, height = ImageUtils.calculate_scaled_size(
         original_width=original_width,
         original_height=original_height,
         height=new_height,
     )
-    assert height == new_height
     assert width == expected_width
+    assert height == new_height
 
 
-def test_image_dimensions_should_be_scaled_correctly_with_smaller_width():
-    original_width, original_height = 1536, 2048
-    new_width, expected_height = 384, 512
-
-    width, height = ImageUtils.calculate_scaled_size(
-        original_width=original_width, original_height=original_height, width=new_width
-    )
-    assert height == expected_height
-    assert width == new_width
-
-
-def test_image_dimensions_should_be_scaled_correctly_with_smaller_height():
-    original_width, original_height = 2048, 1536
-    expected_width, new_height = 512, 384
-
+@pytest.mark.parametrize("original_width, original_height, new_width, new_height", testdata)
+def test_image_dimensions_should_be_scaled_correctly_with_all_params(original_width, original_height, new_width, new_height):
     width, height = ImageUtils.calculate_scaled_size(
         original_width=original_width,
         original_height=original_height,
         height=new_height,
     )
+    assert width == new_width
     assert height == new_height
-    assert width == expected_width
