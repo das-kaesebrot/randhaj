1179d2820de93e573682941d9063d74ac50ec3f7
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-18 15:44:36 +0200

    add cached metadata sqlalchemy model

--- api/models.py
+++ api/models.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel
+from datetime import datetime
+from sqlalchemy import DateTime
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.sql import func
+
+from api.classes import ImageMetadata
 
 
-class ImageResponse(BaseModel):
-    id: str
-    url: str
+class Base(DeclarativeBase):
+    pass
+
+
+class CachedImageMetadata(Base):
+    __tablename__ = "cached_image"
+    id: Mapped[str] = mapped_column(primary_key=True)
+    filename: Mapped[str] = mapped_column()
+    image_metadata: Mapped[ImageMetadata] = mapped_column()
+    created: Mapped[datetime] = mapped_column(DateTime, default=func.now())
