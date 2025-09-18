ef7159f9d785fcd6b3ef9bbfee17e61df9625216
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2025-09-18 16:42:12 +0200

    feature: move ImageMetadata to mapped models as a 1 to 1 relationship

--- api/classes.py
+++ api/classes.py
@@ -1,37 +1,5 @@
 from dataclasses import dataclass
-from typing import Union
 from fastapi.responses import Response
-from PIL import Image
-
-from api.utils.filename import FilenameUtils
-
-
-@dataclass
-class ImageMetadata:
-    original_width: int
-    original_height: int
-    media_type: str
-    format: str
-
-    def get_filename(
-        self, id: str, scaled_width: Union[int, None], scaled_height: Union[int, None]
-    ) -> str:
-        width = self.original_width if not scaled_width else scaled_width
-        height = self.original_height if not scaled_height else scaled_height
-
-        return FilenameUtils.get_filename(
-            id=id, width=width, height=height, format=self.format
-        )
-
-    @staticmethod
-    def from_image(image: Image.Image) -> "ImageMetadata":
-        return ImageMetadata(
-            original_width=image.width,
-            original_height=image.height,
-            media_type=Image.MIME.get(image.format.upper()),
-            format=image.format,
-        )
-
 
 @dataclass
 class TemplateResolutionMetadata:
--- api/models.py
+++ api/models.py
@@ -1,20 +1,33 @@
-from datetime import datetime
-from sqlalchemy import DateTime
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.sql import func
-
-from api.classes import ImageMetadata
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class CachedImageMetadata(Base):
+class CachedImage(Base):
     __tablename__ = "cached_image"
     id: Mapped[str] = mapped_column(primary_key=True)
-    filename: Mapped[str] = mapped_column()
-    image_metadata: Mapped[ImageMetadata] = mapped_column()
-    created: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    original_filename: Mapped[str] = mapped_column(unique=True)
+    # Define 1-to-1 relationship
+    image_metadata: Mapped["ImageMetadata"] = relationship(
+        back_populates="image",
+        cascade="all, delete-orphan"
+    )
+    
+class ImageMetadata(Base):
+    __tablename__ = "image_metadata"
+    
+    id: Mapped[str] = mapped_column(
+        ForeignKey("cached_image.id"),
+        primary_key=True
+    )
+
+    original_width: Mapped[int] = mapped_column()
+    original_height: Mapped[int] = mapped_column()
+    media_type: Mapped[str] = mapped_column()
+    format: Mapped[str] = mapped_column()
+    
+    image: Mapped[CachedImage] = relationship(back_populates="image_metadata")
+    
