04000dfb8de9d4db9718556b51e862c6da7b380e
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2026-03-11 17:49:02 +0100

    init db in main

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/das-kaesebrot/randhaj/database"
 )
 
 func main() {
+	if err := database.Init("sqlite:///"); err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
